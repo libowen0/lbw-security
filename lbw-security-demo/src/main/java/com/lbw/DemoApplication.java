6a2d01db8a9833befea8ba031716edc94221b528
libowen <libw@nroad.com.cn>
2018-10-10 11:25:20 +0800

    add application

---
+++ lbw-security-demo/src/main/java/com/lbw/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.lbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author by lbw , Date on 2018/10/9.
+ */
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class,args);
+  }
+
+  @RequestMapping("/index")
+  public String hello(){
+    return "hello";
+  }
+}
