988fcacb77f70c3b9185e240da2f3fc5f36dc891
libowen <libw@nroad.com.cn>
2018-10-11 18:16:12 +0800

    update readme

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ lbw-security  主模块
 ├─ lbw-security-core  核心业务逻辑  
 ├─ lbw-security-demo  样例程序  
 └─ pom.xml  
+
+
+Spring Security 核心原理
+基于过滤器链，先后依次调用
+UsernamePasswordAuthenticationFiler 处理表单登陆
+BasicAuthenticationFilter  处理basic登陆
+...
+ExceptionTranslationFilter 对最后一步抛出异常进行捕获并下一步处理
+FilterSecurityInterceptor 依据配置来进行放行api
--- lbw-security-browser/src/main/java/com/lbw/BrowserSecurityConfig.java
+++ lbw-security-browser/src/main/java/com/lbw/BrowserSecurityConfig.java
@@ -41,13 +41,6 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .csrf().disable();
 
-//    Spring security 基于过滤器链，先后依次调用
-//    UsernamePasswordAuthenticationFiler 处理表单登陆
-//    BasicAuthenticationFilter  处理basic登陆
-//    ...
-//    ExceptionTranslationFilter 对最后一步抛出异常进行捕获并下一步处理
-//    FilterSecurityInterceptor 依据配置来进行放行api
-
 //    api安全方式
 //    1. 访问api FilterSecurityInterceptor 拦截未认证，抛出异常
 //    2. ExceptionTranslationFilter 捕获异常 并跳转到/login
