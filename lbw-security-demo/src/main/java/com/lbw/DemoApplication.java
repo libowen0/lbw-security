c79e015f175b131dd288ab35d17bc436ab6947ad
libowen <libw@nroad.com.cn>
2018-11-01 15:21:37 +0800

    fixing

--- lbw-security-demo/src/main/java/com/lbw/DemoApplication.java
+++ lbw-security-demo/src/main/java/com/lbw/DemoApplication.java
@@ -1,11 +1,7 @@
 package com.lbw;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,18 +14,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 //})
 
-@EnableJdbcRepositories
 @SpringBootApplication
 @RestController
 @EnableSwagger2
 public class DemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class,args);
+    SpringApplication.run(DemoApplication.class, args);
   }
 
   @RequestMapping("/index")
-  public String hello(){
+  public String hello() {
     return "hello";
   }
 }
